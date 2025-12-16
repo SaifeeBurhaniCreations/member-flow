dfce23cf8b48b292506413042235b209b6fbd911
sadiq53 <jafarussadiq098@gmail.com>
2025-12-16 16:02:33 +0530

    first

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  },
+  // For Vite's preview server in production build
+  preview: {
+    port: 8080,
+    host: "::",
+  },
 }));
